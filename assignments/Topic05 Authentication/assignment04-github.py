d3ee89e04e3584367137f29205689c07f421799f
Ciaran Moran <ciaranmoran@gmail.com>
2024-03-18 21:44:47 +0000

    Attempting to get repo access working

--- assignments/Topic05 Authentication/assignment04-github.py
+++ assignments/Topic05 Authentication/assignment04-github.py
@@ -12,20 +12,75 @@ import requests
 import json
 from config import config as cfg
 
-filename = "repos-private.json"
+from github import Github
+from config import config as cfg
 
-url = 'https://github.com/cpjm/WSAA-coursework/tree/main/assignments/Topic05%20Authentication'
+import os
+# had to install the git library for this
+# using pip install GitPython 
+# info from https://bobbyhadz.com/blog/python-no-module-named-git
+from git import Repo
+
 
 # the more basic way of setting authorization
 #headers = {'Authorization': 'token ' + apikey}
 #response = requests.get(url, headers= headers)
 
 apikey = cfg["githubkey"]
-response = requests.get(url, auth = ('token', apikey))
 
-print (response.status_code)
-#print (response.json())
+#response = requests.get(url, auth = ('token', apikey))
+#print (response.status_code)
+##print (response.json())
+#with  open(filename, 'w') as fp:
+#    repoJSON = response.json()
+#    json.dump(repoJSON, fp, indent=4)
 
-with  open(filename, 'w') as fp:
-    repoJSON = response.json()
-    json.dump(repoJSON, fp, indent=4)
+# Following code based on re-usued code and ideas from:-
+# https://stackoverflow.com/questions/50071841/how-to-push-local-files-to-github-using-python-or-post-a-commit-via-python
+# https://stackoverflow.com/questions/67706647/how-to-add-commit-and-push-a-repository-via-git-python
+# https://stackoverflow.com/questions/74857784/how-to-add-python-files-to-a-new-repository-in-github
+# https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
+# https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#update-a-file
+
+# Test access and list repo names
+g = Github(apikey)
+for repo in g.get_user().get_repos():
+ print(repo.name)
+
+# Open my git repository
+#url = 'https://github.com/cpjm/WSAA-coursework/tree/main/assignments/Topic05%20Authentication/'
+url = 'https://github.com/cpjm/WSAA-coursework/tree/main/assignments/Topic05 Authentication/'
+#url = 'assignments/Topic05%20Authentication/'
+filename = "testfile.txt"
+
+repo = Repo(url)
+
+# Fetch latest changes
+origin = repo.remotes.origin
+origin.fetch()
+
+# Checkout to master branch
+repo.git.checkout('master')
+
+# Replace text
+# with help from https://stackoverflow.com/questions/4128144/replace-string-within-file-contents
+#
+file_path = os.path.join(url, filename)
+with open(file_path, 'r') as file:
+        file_data = file.read()
+
+file_data = file_data.replace('Andrew', 'Ciaran')
+
+with open(file_path, 'w') as file:
+    file.write(file_data)
+
+# Stage changes
+repo.index.add([filename])
+
+# Commit changes
+repo.index.commit("Replace all instances of 'Andrew' with 'Ciaran'")
+print("Changes have been committed")
+
+# Push changes
+origin.push()
+print("Changes have been pushed")
