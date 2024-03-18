a06bd16c01c2dcb5579fc2c00c56df1108909cb8
Ciaran Moran <ciaranmoran@gmail.com>
2024-03-18 19:36:41 +0000

    Assignment 04 - initial commit

---
+++ assignments/Topic05 Authentication/assignment04-github.py
@@ -0,0 +1,32 @@
+#
+# Ciaran Moran
+# G00426050@atu.ie
+# Topic05 Authentication
+# Assignment: Assignment 04 
+# Description: Write a program in python that will read a file from a repository, 
+#           The program should then replace all the instances of the text "Andrew" with your name. 
+#           The program should then commit those changes and push the file back to the repository (You will need authorisation to do this).
+# Based on re-used code from https://github.com/andrewbeattycourseware/wsaa-course-material/tree/main/code/Topic05-authentication/labs
+#
+import requests
+import json
+from config import config as cfg
+
+filename = "repos-private.json"
+
+#url = 'https://api.github.com/repos/andrewbeattycourseware/datarepresentation/contents/code'
+url = 'https://api.github.com/repos/andrewbeattycourseware/aprivateone'
+
+# the more basic way of setting authorization
+#headers = {'Authorization': 'token ' + apikey}
+#response = requests.get(url, headers= headers)
+
+apikey = cfg["githubkey"]
+response = requests.get(url, auth = ('token', apikey))
+
+print (response.status_code)
+#print (response.json())
+
+with  open(filename, 'w') as fp:
+    repoJSON = response.json()
+    json.dump(repoJSON, fp, indent=4)
